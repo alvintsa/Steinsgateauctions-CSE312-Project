ed1a900de566907f86e92e55a154311d8ac62a3e
NishFish <ngopinat@buffalo.edu>
2022-11-09 10:40:36 -0500

    socket imports, no implementation yet

--- app.py
+++ app.py
@@ -2,34 +2,55 @@ from flask import Flask, render_template, send_file
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-#just making sure framework is installed properly
-#execute python app.py
-#may need to update interpreter to venv
+
+# just making sure framework is installed properly
+# execute python app.py
+# may need to update interpreter to venv
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
 
 @app.route('/')
 def home_page():
     return render_template('home.html')
 
-@app.route('/home.css') 
+
+@app.route('/auction')
+def auction_page():
+    return render_template('auctionPage.html')
+
+
+@app.route('/home.css')
 def home_css():
-    return send_file('templates/home.css',mimetype="text/css")
+    return send_file('templates/home.css', mimetype="text/css")
+
 
 @app.route('/login')
 def login_page():
     return render_template('login.html')
 
-@app.route('/login.css') 
+
+@app.route('/login.css')
 def login_css():
-    return send_file('templates/login.css',mimetype="text/css")
+    return send_file('templates/login.css', mimetype="text/css")
+
 
 @app.route('/dog.jpg')
 def ret_dog():
-    return send_file("images/dog.jpg",mimetype="image/gif")
+    return send_file("images/dog.jpg", mimetype="image/gif")
+
 
 @app.route('/backdrop.jpg')
 def ret_backdrop():
-    return send_file("images/backdrop.jpg",mimetype="image/gif")
+    return send_file("images/backdrop.jpg", mimetype="image/gif")
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port='16969')
+    socketio.run(app)
+    # app.run(debug=True, host='0.0.0.0', port='16969')
