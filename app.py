037187e122201f43e2f73e2171c417c00fea17a2
Alvin <alvintsa@buffalo.edu>
2026-01-09 14:34:12 -0500

    Updated cron route

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def escapeHTML(input):
     return input.replace('&', "&amp;").replace('<', "&lt").replace('>', "&gt")
 @app.route("/cron")
 def cron():
-    return { "", 204 }
+    return make_response("", 204)
 
 @app.route('/')
 def home_page():
